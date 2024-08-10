ce03796247687bbcadf8091d71db8525d35014fc
Pritika-Shukla <pritikashukla21@gmail.com>
2024-08-11 01:24:51 +0530

    learned middlewares

--- week3.1/middlewear.js
+++ week3.1/middlewear.js
@@ -47,20 +47,54 @@
 //   console.log("Server is running on port 3000");
 // });
 
+// const express = require("express");
+// const app = express();
+// let noOfReq=0;
+// function calcReq(req,res,next){
+//     noOfReq++;
+//     console.log(noOfReq);
+//     next();
+// }
+// app.use(calcReq)
+
+// app.post("/health-checkup", function (req, res) {
+
+// });
+// app.listen(3000, () => {
+//     console.log("Server is running on port 3000");
+//   });
+
+//Input Validation
 
 const express = require("express");
 const app = express();
-let noOfReq=0;
-function calcReq(req,res,next){
-    noOfReq++;
-    console.log(noOfReq);
-    next();
-}
-app.use(calcReq)
+const zod=require("zod")
+const schema=zod.array(zod.number())
 
+const schema=zod.object({
+    email:zod.string(),
+    password:zod.string(),
+    country:zod.literal("IN").or(zod.literal("US")),
+    kidneys:zod.array(zod.number())
+})
+app.use(express.json());
 app.post("/health-checkup", function (req, res) {
+const kidneys=req.body.kidneys;
+const response=schema.safeParse(kidneys)
+// const KidneyLength=kidneys.length;
 
-});
+// res.send("your kidney is "+ KidneyLength)
+res.send({
+    response
+})
+ });
+
+ //gloabl catches
+//  app.use(function(err,req,res,next){
+//     res.json({
+//         msg:"something went wrong bro"
+//     })
+//  })
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-  });
+  console.log("Server is running on port 3000");
+});
