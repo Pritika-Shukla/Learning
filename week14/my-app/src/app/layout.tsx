24865e44697c290cde7a9a7ebeb79eead2b9aeba
Pritika-Shukla <pritikashukla21@gmail.com>
2024-09-23 11:10:47 +0530

    completed 14.1

--- week14/my-app/src/app/layout.tsx
+++ week14/my-app/src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        hiiiiiiiiiiiii
         {children}
       </body>
     </html>
