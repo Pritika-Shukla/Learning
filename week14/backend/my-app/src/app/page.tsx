6447a5cae0a3706f12363a7c11132dbec620d9cd
Pritika-Shukla <pritikashukla21@gmail.com>
2024-10-02 22:48:35 +0530

    backend in nextjs

---
+++ week14/backend/my-app/src/app/page.tsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+
+async function fetchData() {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/todos/2"
+  );
+
+
+  return response.data;
+}
+
+export default async function Home() {
+  const data = await fetchData();
+  return (
+    <div>
+      bhhjgorhsdgohejrso  
+      <h1>{data.title}</h1>
+    </div>
+  );
+}
